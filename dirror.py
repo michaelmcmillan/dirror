daefc56adbf3cca1c0e8420c313022f842eb96a5
Michael McMillan <email@michaelmcmillan.net>
2017-04-17 16:28:44 +0200

    extracted Sweeper class for finding directories recursively. also documented each class

--- dirror.py
+++ dirror.py
@@ -2,55 +2,74 @@
 import os, sys, logging
 from argparse import ArgumentParser
 
-class MirrorCheck:
+class Sweeper:
+    '''Recursively generates tree from directory structure.'''
 
-    def __init__(self, should_log=False):
-        self.log = logging.getLogger(__name__)
-        self.log.disabled = not should_log
-
-    def find_directories(self, path):
+    @staticmethod
+    def find_directories(path):
         files_and_directories = os.listdir(path)
         return [potential_dir for potential_dir in files_and_directories \
             if os.path.isdir(os.path.join(path, potential_dir))]
 
-    def recursively_find_directories(self, path):
+    @staticmethod
+    def recursively_find_directories(path):
         tree = {}
-        for directory in self.find_directories(path):
+        for directory in Sweeper.find_directories(path):
             absolute_path = os.path.join(path, directory)
-            tree[directory] = self.recursively_find_directories(absolute_path)
+            tree[directory] = Sweeper.recursively_find_directories(absolute_path)
         return tree 
 
-    def is_reflection(self, tree, other_tree, prefix=None, exclusions=[]):
-        def identical_trees(tree, other_tree, prefix):
-            for directory in (tree.keys() - exclusions):
-                prefixed_directory = (prefix or '') + directory
-                identical_trees(tree[directory], other_tree[prefixed_directory], prefix)
+class Dirror:
+    '''Finds asymmetries between directory trees.'''
 
-        try:
-            identical_trees(tree, other_tree, prefix)
-            return True
-        except KeyError as error:
-            self.log.error('%s broke the mirror.' % error)
-            return False
+    def __init__(self, prefix=None, appendix=None, exclusions=None):
+        self.prefix = prefix or ''
+        self.appendix = appendix or ''
+        self.exclusions = exclusions or []
 
-    def determine(self, directory_path, other_directory_path, prefix, exclusions):
-        tree = self.recursively_find_directories(directory_path)
-        other_tree = self.recursively_find_directories(other_directory_path)
-        return self.is_reflection(tree, other_tree, prefix, exclusions)
+    def find_shards(self, tree, other_tree, shards):
+        for directory in (tree.keys() - self.exclusions):
+            test_directory = self.prefix + directory + self.appendix
 
-def parse_args(args):
-    parser = ArgumentParser(prog='dirror.py',
-                            description='Ensure that two directories mirror each other.')
-    parser.add_argument('source_directory', type=str, help='path to source directory')
-    parser.add_argument('test_directory', type=str, help='path to test directory')
-    parser.add_argument('prefix', type=str, help='prefix for subddirectories in the test directory')
-    parser.add_argument('exclude', type=lambda s: [directory for directory in s.split(',')], help='comma delimited list of exlcuded directory names')
+            try:
+                self.find_shards(tree[directory], other_tree[test_directory], shards)
+            except KeyError as error:
+                shard = str(error)[1:-1]
+                shards.append(shard)
+        return shards
 
-    return parser.parse_args(args)
+    def determine(self, directory_path, other_directory_path):
+        tree = Sweeper.recursively_find_directories(directory_path)
+        other_tree = Sweeper.recursively_find_directories(other_directory_path)
+        return self.find_shards(tree, other_tree, [])
+
+class CommandLine:
+    '''Interfaces Dirror with the command line.'''
+
+    NAME = 'dirror.py'
+    DESCRIPTION = 'Ensure that two directories mirror each other.'
+    ARGUMENTS = (
+        {'flag': 'source_directory', 'type': str, 'help': 'path to source directory'},
+        {'flag': 'test_directory', 'type': str, 'help': 'path to test directory'},
+        {'flag': 'prefix', 'type': str, 'help': 'prefix for subddirectories in the test directory'},
+        {'flag': 'exclude', 'type': lambda paths: [path for path in paths.split(',')], 'help': 'comma delimited list of exlcuded directory names'}
+    )
+
+    def __init__(self, raw_arguments):
+        self.raw_arguments = raw_arguments[1:]
+
+    def parse_arguments(self):
+        parser = ArgumentParser(prog=self.NAME, description=self.DESCRIPTION)
+        for argument in self.ARGUMENTS:
+            parser.add_argument(argument['flag'], type=argument['type'], help=argument['help'])
+        return parser.parse_args(self.raw_arguments)
 
 if __name__ == '__main__':
-    args = parse_args(sys.argv[1:])
-    mirror_check = MirrorCheck(should_log=True)
-    mirrored = mirror_check.determine(args.source_directory, args.test_directory, prefix=args.prefix, exclusions=args.exclude)
-    signal = int(mirrored)
-    sys.exit(signal)
+    arguments = CommandLine(sys.argv).parse_arguments()
+    dirror = Dirror(prefix=arguments.prefix, exclusions=arguments.exclude)
+
+    shards = dirror.determine(arguments.source_directory, arguments.test_directory)
+    for shard in shards:
+        logging.getLogger('dirror').error('%s broke the mirror.' % shard)
+
+    sys.exit(1 if any(shards) else 0)
--- test/test_dirror.py
+++ test/test_dirror.py
@@ -1,90 +1,98 @@
-from unittest import TestCase, main
-from dirror import MirrorCheck
+from unittest import TestCase, main, skip
+from subprocess import check_output
+from dirror import Dirror, Sweeper
 
 class TestFindDirectories(TestCase):
 
     def test_it_returns_single_subdirectory(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/one-subdirectory'
-        directories = mirror.find_directories(path)
+        directories = sweeper.find_directories(path)
         self.assertEqual(directories, ['subdirectory'])
 
     def test_it_returns_empty_list_if_no_subdirectories(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/no-subdirectory'
-        directories = mirror.find_directories(path)
+        directories = sweeper.find_directories(path)
         self.assertEqual(directories, [])
 
     def test_it_returns_empty_list_if_only_contains_files(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/one-file'
-        directories = mirror.find_directories(path)
+        directories = sweeper.find_directories(path)
         self.assertEqual(directories, [])
 
 class TestRecursiveFindDirectories(TestCase):
 
     def test_it_returns_a_dictionary_of_directory_contents(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/one-subdirectory-within-another-subdirectory'
-        directories = mirror.recursively_find_directories(path)
+        directories = sweeper.recursively_find_directories(path)
         self.assertEqual(directories, {'subdirectory': {'another-subdirectory': {}}})
 
     def test_it_returns_an_empty_dictionary_if_no_subdirectories(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/no-subdirectory'
-        directories = mirror.recursively_find_directories(path)
+        directories = sweeper.recursively_find_directories(path)
         self.assertEqual(directories, {})
 
     def test_it_returns_a_dictionary_of_multiple_directories(self):
-        mirror = MirrorCheck()
+        sweeper = Sweeper()
         path = 'test/fixtures/two-subdirectories-within-a-subdirectory'
-        directories = mirror.recursively_find_directories(path)
+        directories = sweeper.recursively_find_directories(path)
         self.assertEqual(directories, {'first-directory': {}, 'second-directory': {}})
 
 class TestCompare(TestCase):
 
-    def test_it_returns_true_if_both_trees_are_empty(self):
-        mirror = MirrorCheck()
+    def test_it_returns_empty_list_if_both_trees_are_empty(self):
+        dirror = Dirror()
         first_tree = {}
         second_tree = {}
-        reflection = mirror.is_reflection(first_tree, second_tree)
-        self.assertTrue(reflection)
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, [])
 
-    def test_it_returns_false_if_one_tree_has_root_the_other_does_not(self):
-        mirror = MirrorCheck()
+    def test_it_returns_shard_if_one_tree_has_root_the_other_does_not(self):
+        dirror = Dirror()
         first_tree = {'root': {}}
         second_tree = {}
-        reflection = mirror.is_reflection(first_tree, second_tree)
-        self.assertFalse(reflection)
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, ['root'])
 
-    def test_it_returns_true_if_prefixed_directory_is_in_the_other_tree(self):
-        mirror = MirrorCheck()
+    def test_it_returns_empty_list_if_prefixed_directory_is_in_the_other_tree(self):
+        dirror = Dirror(prefix='test_')
         first_tree = {'root': {}}
         second_tree = {'test_root': {}}
-        reflection = mirror.is_reflection(first_tree, second_tree, prefix='test_')
-        self.assertTrue(reflection)
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, [])
+    
+    def test_it_returns_empty_list_if_appendixed_directory_is_in_the_other_tree(self):
+        dirror = Dirror(appendix='_test')
+        first_tree = {'root': {}}
+        second_tree = {'root_test': {}}
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, [])
 
-    def test_it_returns_false_if_leafs_dont_match(self):
-        mirror = MirrorCheck()
+    def test_it_returns_shard_if_leafs_dont_match(self):
+        dirror = Dirror()
         first_tree = {'root': {'leaf': {}}}
-        second_tree = {'test_root': {'test_liif': {}}}
-        reflection = mirror.is_reflection(first_tree, second_tree, prefix='test_')
-        self.assertFalse(reflection)
+        second_tree = {'root': {'liif': {}}}
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, ['leaf'])
 
     def test_it_ignores_excluded_directories(self):
-        mirror = MirrorCheck()
+        dirror = Dirror(exclusions=['__pycache__'])
         first_tree = {'__pycache__': {}}
         second_tree = {}
-        reflection = mirror.is_reflection(first_tree, second_tree, prefix='test_', exclusions=['__pycache__'])
-        self.assertTrue(reflection)
+        shards = dirror.find_shards(first_tree, second_tree, [])
+        self.assertEqual(shards, [])
 
 class TestSystem(TestCase):
 
-    def test_it_returns_true_if_test_has_dir_for_every_src(self):
-        mirror = MirrorCheck()
-        src_path, test_path = 'test/fixtures/system/src', 'test/fixtures/system/test'
-        src_mirrors_test = mirror.determine(src_path, test_path, 'test_', [])
-        self.assertTrue(src_mirrors_test)
+    def test_it_exits_silently_if_no_shards_found(self):
+        flags = ('test/fixtures/system/src', 'test/fixtures/system/test', 'test_', '__pycache__')
+        command = 'python3 dirror.py %s' % ' '.join(flags)
+        output = check_output(command, shell=True)
+        self.assertEqual(output, b'')
 
 if __name__ == '__main__':
     main()
